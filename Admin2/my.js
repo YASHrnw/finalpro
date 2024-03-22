3030b8828188498ce47c869b4b46da612dc467c0
YASHrnw <f347yash@gmail.com>
2024-03-22 19:55:10 +0530

    add

---
+++ Admin2/my.js
@@ -0,0 +1,40 @@
+const arr = [];
+
+//insert opp
+const addData = () => {
+
+    let allData = JSON.parse(localStorage.getItem("CatInfo"));
+    let len = allData != null ? allData.length : 0;
+    const name = document.catfrm.catname.value;
+    const catData = {
+        id: len + 1,
+        name: name
+    }
+    arr.push(catData);
+    localStorage.setItem("CatInfo", JSON.stringify(arr));
+    document.catfrm.catname.value = '';
+    dispCat()
+}
+
+// display cat
+const dispCat = () => {
+    //  allCatData
+    let tr = '';
+    
+    let allData = JSON.parse(localStorage.getItem("CatInfo"));
+    if (allData != null) {
+        allData.map((i) => {
+            tr += `<tr>
+                     <td>${i.id}</td> 
+                     <td> ${i.name} </td> 
+                      <td><a href ="#" class="btn btn-danger" onclick="delData(${i.id})">Delete</a></td>
+                   
+            </tr>`;
+
+        })
+
+        document.getElementById("allCatData").innerHTML = tr;
+
+    }
+
+}
